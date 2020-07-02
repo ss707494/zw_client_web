b3f3e1cac5c7b60fbb1df73978f3cb3419590be8
shensheng <ss707494@163.com>
2020-07-02 12:56:05 +0800

    static html

--- pages/group/[appModule].tsx
+++ pages/group/[appModule].tsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import {AppModuleTypeEnum} from '../../utils/ss_common/enum'
 import {HomeAppModule, HomeType} from '../../utils/view/home/appModule'
-import {init} from '../home/[appModule]'
 
 export default HomeAppModule(HomeType.group)
 
-export const getStaticPaths = () => {
-  return {
-    paths: [AppModuleTypeEnum.categorySelection, AppModuleTypeEnum.lineRanking, AppModuleTypeEnum.topRanking].map(v => ({
-      params: {
-        appModule: v,
-      },
-    })),
-    fallback: true,
-  }
-}
-export const getStaticProps = init
+// export const getStaticPaths = () => {
+//   return {
+//     paths: [AppModuleTypeEnum.categorySelection, AppModuleTypeEnum.lineRanking, AppModuleTypeEnum.topRanking].map(v => ({
+//       params: {
+//         appModule: v,
+//       },
+//     })),
+//     fallback: true,
+//   }
+// }
+// export const getStaticProps = init
